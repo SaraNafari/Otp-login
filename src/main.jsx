7cd4d0cb529c5c5eb7074e2ab11b3342b8e61164
Sara Nafari <saranafariwork6@gmail.com>
2025-12-19 19:31:00 +0330

    Apply Vazirmatn font, fix bold headings, and update styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './styles/fonts.css'    //  فونت
+import "./styles/auth.css"    //  سایر استایل‌ها
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
